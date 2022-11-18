ee022b1eaa04b92c072385d91b1af2e84cf7a3b2
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2022-11-19 08:32:47 +0900

    create: create component/atoms

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { Button } from '.';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args: React.ComponentProps<typeof Button>) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  href: "/",
+  children: "Button",
+};
---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "./Button.module.scss";
+
+type ButtonProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+export const Button = ({ href, children }: ButtonProps) => {
+  return (
+    <Link href={href}>
+      <a className={styles.button}>
+        {children}
+      </a>
+    </Link>
+  );
+};
