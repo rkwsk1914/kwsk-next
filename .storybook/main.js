9d1bde9dc230608edf936e08f7f88884f6c0a3d2
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2022-11-21 11:11:49 +0900

    fix: fix storybook scss.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-postcss',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+    '@storybook/preset-scss',
+    '@storybook/addon-actions',
   ],
   framework: '@storybook/react',
   core: {
