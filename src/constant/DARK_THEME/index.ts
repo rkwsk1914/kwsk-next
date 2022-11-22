7c325d25516c47e083af087b8a7aaef52448bfe1
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2022-11-23 00:53:39 +0900

    add: constant DARK_THEME

---
+++ src/constant/DARK_THEME/index.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles';
+
+export const DARK_THEME = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#cecece',
+    },
+  },
+});
---
+++ src/constant/index.ts
@@ -0,0 +1,1 @@
+export * from './DARK_THEME'
